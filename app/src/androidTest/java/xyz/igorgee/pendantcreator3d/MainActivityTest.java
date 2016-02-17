5c5dcf4c572c2db9c9262a42bcafecad5025d8cf
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-02-17 16:22:10 -0500

    Set up Espresso testing

---
+++ app/src/androidTest/java/xyz/igorgee/pendantcreator3d/MainActivityTest.java
@@ -0,0 +1,27 @@
+package xyz.igorgee.pendantcreator3d;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+//        onView(withText("Hello World!")).check(matches(isDisplayed()));
+    }
+}
