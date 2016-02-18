6867185f3e89ead3ae20be3dbb4211c038bdb503
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-02-18 12:05:54 -0500

    Turn LogIn UI into a fragment

---
+++ app/src/main/java/xyz/igorgee/pendantcreator3d/LogInFragment.java
@@ -0,0 +1,17 @@
+package xyz.igorgee.pendantcreator3d;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LogInFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
+    }
+}
--- app/src/main/java/xyz/igorgee/pendantcreator3d/MainActivity.java
+++ app/src/main/java/xyz/igorgee/pendantcreator3d/MainActivity.java
@@ -1,13 +1,130 @@
 package xyz.igorgee.pendantcreator3d;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Bind(R.id.drawerLayout) DrawerLayout drawerLayout;
+    @Bind(R.id.rootLayout) CoordinatorLayout rootLayout;
+    @Bind(R.id.toolBar) Toolbar toolbar;
+    @Bind(R.id.navigation) NavigationView navigation;
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentPlaceholder, new LogInFragment());
+        fragmentTransaction.commit();
+
+        ButterKnife.bind(this);
+        initializeInstances();
+    }
+
+    private void initializeInstances() {
+        setSupportActionBar(toolbar);
+        drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout,
+                R.string.drawer_open_desc, R.string.drawer_close_desc);
+        drawerLayout.setDrawerListener(drawerToggle);
+        navigation.setNavigationItemSelectedListener(this);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.app_name);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.spades:
+                Snackbar.make(rootLayout, R.string.spades_string, Snackbar.LENGTH_SHORT)
+                        .setAction("Ok", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+                break;
+            case R.id.hearts:
+                Snackbar.make(rootLayout, R.string.hearts_string, Snackbar.LENGTH_SHORT)
+                        .setAction("Ok", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+                break;
+            case R.id.clovers:
+                Snackbar.make(rootLayout, R.string.clovers_string, Snackbar.LENGTH_SHORT)
+                        .setAction("Ok", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+                break;
+            case R.id.diamonds:
+                Snackbar.make(rootLayout, R.string.diamonds_string, Snackbar.LENGTH_SHORT)
+                        .setAction("Ok", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+                break;
+
+            case R.id.custom:
+                Snackbar.make(rootLayout, R.string.custom_string, Snackbar.LENGTH_SHORT)
+                        .setAction("Ok", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Changes home icon from "back" to "hamburger"
+        drawerToggle.syncState();
     }
 }
