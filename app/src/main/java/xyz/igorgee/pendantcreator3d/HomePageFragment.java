656a632b5e2c685a8e00b03e54e7aba8dbe2b118
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-02-24 17:52:06 -0500

    Add Homepage

---
+++ app/src/main/java/xyz/igorgee/pendantcreator3d/HomePageFragment.java
@@ -0,0 +1,22 @@
+package xyz.igorgee.pendantcreator3d;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public class HomePageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_homepage, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+}
