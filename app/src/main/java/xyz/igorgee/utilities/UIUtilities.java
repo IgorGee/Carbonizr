476cf10f4539c6d71bdba13699f6d538b092d06e
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-02-29 17:43:25 -0500

    Add AlertDialog Utility

--- app/src/main/java/xyz/igorgee/utilities/UIUtilities.java
+++ app/src/main/java/xyz/igorgee/utilities/UIUtilities.java
@@ -1,9 +1,13 @@
 package xyz.igorgee.utilities;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 
 public class UIUtilities {
+    
     public static void makeSnackbar(View view, int stringID) {
         makeSnackbar(view, stringID, null);
     }
@@ -30,5 +34,66 @@ public class UIUtilities {
 
                     }
                 }).show();
+    }
+
+    public static void makeAlertDialog(Context context, String message, int iconID) {
+        new AlertDialog.Builder(context)
+                .setTitle("Image Information")
+                .setMessage(message)
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setIcon(iconID)
+                .show();
+    }
+
+    public static void makeAlertDialog(Context context, String message) {
+        new AlertDialog.Builder(context)
+                .setTitle("Image Information")
+                .setMessage(message)
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show();
+    }
+
+    public static void makeAlertDialog(Context context, String message,
+                                       String positiveButton, String negativeButton,
+                                       int iconID) {
+        new AlertDialog.Builder(context)
+                .setTitle("Image Information")
+                .setMessage(message)
+                .setPositiveButton(positiveButton, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton(negativeButton, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setIcon(iconID)
+                .show();
     }
 }
