0165879436d6599e3ce34f4ce3e4d9b24a16712d
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-03-16 14:22:35 -0400

    Replace default AndroidLauncher

--- android/src/xyz/igorgee/imagecreatorg3dx/ObjectViewer.java
+++ android/src/xyz/igorgee/imagecreatorg3dx/ObjectViewer.java
@@ -1,20 +1,22 @@
 package xyz.igorgee.imagecreatorg3dx;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-import xyz.igorgee.imagecreator3d.MainActivity;
-import xyz.igorgee.imagecreator3d.MyGDXClass;
+import java.io.File;
+
+import xyz.igorgee.imagecreator3d.ModelViewer;
 
 public class ObjectViewer extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		File object = (File) getIntent().getExtras().get("model");
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGDXClass(), config);
+		initialize(new ModelViewer(object), config);
 	}
 }
