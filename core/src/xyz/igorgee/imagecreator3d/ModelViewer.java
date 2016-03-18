12158305cb4d924f0c0b4bab87be7f6af5a55ff1
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-03-18 17:04:53 -0400

    Fix rendering issue.
    Fixes #32.

--- core/src/xyz/igorgee/imagecreator3d/ModelViewer.java
+++ core/src/xyz/igorgee/imagecreator3d/ModelViewer.java
@@ -4,7 +4,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.BlendingAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
@@ -68,8 +72,8 @@ public class ModelViewer extends ApplicationAdapter {
         }
 
         if(instance != null){
-            for (Material mat : instance.materials) {
-                mat.set(new BlendingAttribute(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA));
+            for (Material mat : model.materials) {
+                mat.remove(BlendingAttribute.Type);
             }
         }
 
