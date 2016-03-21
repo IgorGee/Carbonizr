7deb9ea47f2c902614c272abb1fdb00867ddf07b
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-03-21 15:48:15 -0400

    Void previous api key and secret.

--- android/src/xyz/igorgee/shapwaysapi/Consumer.java
+++ android/src/xyz/igorgee/shapwaysapi/Consumer.java
@@ -1,8 +1,8 @@
 package xyz.igorgee.shapwaysapi;
 
 public enum Consumer {
-    KEY("b0231f7907e3e7d7a6cb016a92d2d8b7d71fa252"),
-    SECRET("fd704d47663a5c40b38c5a326ad3c7b3f55f42da");
+    KEY("YOUR-KEY-HERE"),
+    SECRET("YOUR-SECRET-HERE");
 
     private String value;
 
