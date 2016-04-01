951ce91e6a1fe64e641926f87d5b3064d92e3aa0
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-04-01 15:31:31 -0400

    Utilize Picasso.
    I should make a note that if I ever spend more than an hour on something, then someone has already solved it for me and I should study up on more libraries that have been created.
    Fixes #48.

--- android/src/xyz/igorgee/imagecreator3d/CustomAdapter.java
+++ android/src/xyz/igorgee/imagecreator3d/CustomAdapter.java
@@ -2,14 +2,9 @@ package xyz.igorgee.imagecreator3d;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,44 +12,30 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import xyz.igorgee.imagecreatorg3dx.ObjectViewer;
-import xyz.igorgee.utilities.ImageHelper;
 import xyz.igorgee.utilities.UIUtilities;
 
 import static xyz.igorgee.utilities.UIUtilities.makeAlertDialog;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
-    //TODO Add Disk Caching as well.
-
-    final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-    final int cacheSize = maxMemory / 8;
 
     private final Context context;
     private final ArrayList<Model> models;
-    private LruCache<String, Bitmap> mMemoryCache;
 
     public CustomAdapter(Context context, ArrayList<Model> models) {
         this.context = context;
         this.models = models;
-
-        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
-            @Override
-            protected int sizeOf(String key, Bitmap bitmap) {
-                // The cache size will be measured in kilobytes rather than
-                // number of items.
-                return bitmap.getByteCount() / 1024;
-            }
-        };
     }
 
     @Override
@@ -70,22 +51,12 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         holder.textView.setText(models.get(position).getName());
         holder.modelDirectory = models.get(position).getLocation();
         File imageLocation = new File(models.get(position).getLocation(), models.get(position).getName() + ".jpg");
-        loadBitmap(imageLocation, holder.imageView);
+        Picasso.with(context).load(imageLocation).into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
         return models.size();
-    }
-
-    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
-        if (getBitmapFromMemCache(key) == null) {
-            mMemoryCache.put(key, bitmap);
-        }
-    }
-
-    public Bitmap getBitmapFromMemCache(String key) {
-        return mMemoryCache.get(key);
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
@@ -166,95 +137,5 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
                 models.get(position).setModelID(modelID);
             }
         }
-    }
-
-    class BitmapWorkerTask extends AsyncTask<File, Void, Bitmap> {
-        private final WeakReference<ImageView> imageViewReference;
-        private File file;
-
-        public BitmapWorkerTask(ImageView imageView) {
-            // Use a WeakReference to ensure the ImageView can be garbage collected
-            imageViewReference = new WeakReference<>(imageView);
-        }
-
-        // Decode image in background.
-        @Override
-        protected Bitmap doInBackground(File... params) {
-            final Bitmap bitmap = ImageHelper.decodeSampledBitmapFromResource(params[0]);
-            addBitmapToMemoryCache(String.valueOf(params[0]), bitmap);
-            return bitmap;
-        }
-
-        // Once complete, see if ImageView is still around and set bitmap.
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            if (isCancelled()) {
-                bitmap = null;
-            }
-
-            if (imageViewReference != null && bitmap != null) {
-                final ImageView imageView = imageViewReference.get();
-                final BitmapWorkerTask bitmapWorkerTask =
-                        getBitmapWorkerTask(imageView);
-                if (this == bitmapWorkerTask && imageView != null) {
-                    imageView.setImageBitmap(bitmap);
-                }
-            }
-        }
-    }
-
-    static class AsyncDrawable extends BitmapDrawable {
-        private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
-
-        public AsyncDrawable(Resources res, Bitmap bitmap, BitmapWorkerTask bitmapWorkerTask) {
-            super(res, bitmap);
-            bitmapWorkerTaskReference = new WeakReference<>(bitmapWorkerTask);
-        }
-
-        public BitmapWorkerTask getBitmapWorkerTask() {
-            return bitmapWorkerTaskReference.get();
-        }
-    }
-
-    public void loadBitmap(File file, ImageView imageView) {
-        final String imageKey = file.getAbsolutePath();
-
-        final Bitmap bitmap = getBitmapFromMemCache(imageKey);
-        if (bitmap != null) {
-            imageView.setImageBitmap(bitmap);
-        } else {
-            imageView.setImageResource(R.drawable.ic_launcher);
-            BitmapWorkerTask task = new BitmapWorkerTask(imageView);
-            task.execute(file);
-        }
-    }
-
-    public static boolean cancelPotentialWork(File file, ImageView imageView) {
-        final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
-
-        if (bitmapWorkerTask != null) {
-            final File bitmapData = bitmapWorkerTask.file;
-            // If bitmapData is not yet set or it differs from the new data
-            if (bitmapData == null || bitmapData != file) {
-                // Cancel previous task
-                bitmapWorkerTask.cancel(true);
-            } else {
-                // The same work is already in progress
-                return false;
-            }
-        }
-        // No task associated with the ImageView, or an existing task was cancelled
-        return true;
-    }
-
-    private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
-        if (imageView != null) {
-            final Drawable drawable = imageView.getDrawable();
-            if (drawable instanceof AsyncDrawable) {
-                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
-                return asyncDrawable.getBitmapWorkerTask();
-            }
-        }
-        return null;
     }
 }
